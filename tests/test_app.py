f9e566eb2c103bfe0b899eb29aeb02ae8df6b6bb
“Nabil <nabil.ahmed@kcl.ac.uk>
2026-09-07 14:40:16 +0100

    Purposefully fail test to check branch protection

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,3 +8,7 @@ client = TestClient(app)
 def test_feed_requires_auth():
     response = client.get("/feed")
     assert response.status_code == 401
+
+def test_fail_requires_auth():
+    response = client.get("/red")
+    assert response.status_code == 401
